61e8862ad8b1f1ce374a8dc825e9d5f9e68005ed
Stanislav Kreyzo <105377326+skreyzo@users.noreply.github.com>
2022-12-16 21:44:12 +0700

    ymap added

--- client/src/components/Contacts/Contact.jsx
+++ client/src/components/Contacts/Contact.jsx
@@ -1,18 +1,33 @@
 import React from "react";
-import { YMaps, Map } from "@pbe/react-yandex-maps";
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
 // import styled from "./contacts.module.css";
 
 const Contacts = () => {
-
   return (
-    <YMaps>
-      <div>My awesome application with maps!
-
-      <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
-
-      </div>
-    </YMaps>
+    <>
+    <div>
+     <p> Где мы находимся</p>
+    </div>
+      <YMaps>
+        <Map
+          defaultState={{
+            center: [56.30339278464996, 38.13734384788514],
+            zoom: 16,
+            controls: ["zoomControl", "fullscreenControl"],
+          }}
+          modules={["control.ZoomControl", "control.FullscreenControl"]}
+        >
+          <Placemark
+            modules={["geoObject.addon.balloon"]}
+            defaultGeometry={[56.30339278464996, 38.13734384788514]}
+            properties={{
+              balloonContentBody: 'магазин "SweetBuket"',
+            }}
+          />
+        </Map>
+      </YMaps>
+    </>
   );
 };
 
